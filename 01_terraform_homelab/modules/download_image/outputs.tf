8f68936916c9a59c047b36db25006bb879945f5d
mikha-22 <milenikaiqbal@gmail.com>
2025-07-01 06:17:58 +0700

    attempt to split 01

--- 01_terraform_homelab/modules/download_image/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "file_id" {
-  description = "The full ID of the downloaded file in Proxmox (e.g., 'local:iso/ubuntu-24.04-cloudimg.img')."
-  value       = proxmox_virtual_environment_download_file.image.id
-}
